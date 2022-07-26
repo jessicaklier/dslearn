81769267a0b6d5aae7fcf0874ad2e055fd86a40b
jessicaklier <jhessicadesouza12@hotmail.com>
2022-07-26 15:11:52 -0300

    Repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repository/OfferRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslearnbds.entities.Offer;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+}
