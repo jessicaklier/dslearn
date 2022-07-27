66747147037919b43675c436b706adac5689d3c5
jessicaklier <jhessicadesouza12@hotmail.com>
2022-07-27 15:54:40 -0300

    User by id

--- backend/src/main/java/com/devsuperior/dslearnbds/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/ResourceServerConfig.java
@@ -2,7 +2,6 @@ package com.devsuperior.dslearnbds.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
